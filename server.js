f3c21330fc392c64be6856e61ed978b64c611923
David L. Whitehurst <dlwhitehurst@gmail.com>
2021-01-25 19:09:53 -0500

    added swagger at /api-docs

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// swagger api documentation
+const swaggerUi = require("swagger-ui-express"), 
+   swaggerDocument = require("./swagger.json");
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to a States API Express application." });
 });
 
 require("./app/routes/state.routes.js")(app);
+
+// swagger path to api documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
